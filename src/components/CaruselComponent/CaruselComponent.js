9c9a186c303a0f673210f34ab53258cc346c0d24
Michael19932k <46903237+Michael19932k@users.noreply.github.com>
2020-01-06 02:30:37 +0200

    minor carusel update

--- src/components/CaruselComponent/CaruselComponent.js
+++ src/components/CaruselComponent/CaruselComponent.js
@@ -14,11 +14,37 @@ class CaruselComponent extends React.Component {
             preLoadImgDisplay: [],
             preLoadTitle: [],
             animate: false,
+            interval: ''
         };
-        this.container = null
+        this.pause = this.pause.bind(this);
+        this.unpause = this.unpause.bind(this);
+        this.doStuff = this.doStuff.bind(this);
     }
 
+    doStuff(params) {
+            // this.setState({ intervalTimer: 2000 })
+            this.interval = setInterval(() => {
+                    let a = this.state.preLoadImg
+                    let b = this.state.titles
+                    var copy = a.pop()
+                    var copy2 = b.pop()
+                    // console.log(copy)
+                    a.unshift(copy);
+                    b.unshift(copy2);
+                    this.setState({
+                        preLoadImgDisplay: a,
+                        titles: b,
+                        animate: true
+                    })
+                    // console.log(this.state.preLoadImgDisplay);
+                    setTimeout(() => {
+                        this.setState({ animate: false })
+                    }, 1000)
 
+            }, 2000);
+    }
+
+    // `${this.state.intervalTimer}`
     componentDidMount() {
         // event.preventDefault()
         // console.log(this.state.value)
@@ -26,62 +52,42 @@ class CaruselComponent extends React.Component {
             .then(res => res.json())
             .then((data) => {
                 this.setState({ movies: data.results })
-                // console.log(this.state.movies)
+                console.log(this.state.movies)
                 let linksArray = []
                 let titlesArray = []
-                for (let i = 0; i < this.state.movies.length; i++) {
-                    let separateLink = `https://image.tmdb.org/t/p/w780/${this.state.movies[i].poster_path}`
+                this.state.movies.forEach((picture) => {
+                    let separateLink = `https://image.tmdb.org/t/p/w780/${picture.poster_path}`
                     linksArray.push(separateLink)
-                    let separateTitle = this.state.movies[i].title
+                    let separateTitle = picture.title
                     titlesArray.push(separateTitle)
-                }
+                })
                 this.setState({ links: linksArray })
                 this.setState({ titles: titlesArray })
-
                 let tempPreload = []
                 this.state.links.forEach((picture) => {
                     const img = new Image();
                     img.src = picture;
-                    tempPreload.push(picture)
-                    this.setState({ preLoadImg: tempPreload })
+                    this.state.preLoadImg.push(picture)
                     // console.log(this.state.preLoadImg)
                 });
-                return
+
             })
             .catch(console.log)
-
-        this.interval = setInterval(() => {
-
-            // this.scrollContainerBy(this.scrollContainerBy((this.container.scrollWidth / 20)))
-            if (this.state.preLoadImg[0]) {
-                // console.log("yes")
-                let a = this.state.preLoadImg
-                let b = this.state.titles
-                var copy = a.pop()
-                var copy2 = b.pop()
-                // console.log(copy)
-                a.unshift(copy);
-                b.unshift(copy2);
-                this.setState({
-                    preLoadImgDisplay: a,
-                    titles: b,
-                    animate: true
-                })
-                // console.log(this.state.preLoadImgDisplay);
-                setTimeout(() => {
-                    this.setState({ animate: false })
-                }, 3000)
-
-            }
-        }, 2000);
-    }
-    scrollContainerBy(distance) {
-        this.container.scrollBy({ left: distance, behavior: 'smooth' })
-        console.log(this.container.scrollWidth / 20)
+            console.log(this.state.preLoadImg)
+            this.doStuff()
     }
 
-    componentWillUnmount() {
+    // componentWillUnmount() {
+    //     clearInterval(this.interval);
+    // }
+    pause(event) {
+        console.log(event.type, 'enter')
         clearInterval(this.interval);
+    }
+    unpause(event) {
+        console.log(event.type, 'leave')
+        this.doStuff()
+
     }
 
 
@@ -90,24 +96,24 @@ class CaruselComponent extends React.Component {
 
     render() {
         return (
-            <div className="caruselControlsWrapper">
+            <div onMouseOver={(event) => this.pause(event)} onMouseOut={(event) => this.unpause(event)} className="caruselControlsWrapper">
                 <button
                     type="button" className="caruselButtonPrevious"
                     onClick={() => {
-                        this.scrollContainerBy(-(this.container.scrollWidth / 20))
+
                     }}
                 >
                     Previous
         </button>
-                <div className="carusel" ref={node => { this.container = node }}>
+                <div className="carusel">
 
                     {
                         this.state.movies.map((movie, i) => {
                             // console.log(this.state.animate, i)
                             return (
-                                <div className={(this.state.animate && i === 0) ? "caruselCardWrapper newCard3" : "caruselCardWrapper"} key={i}>
-                                    <div className={(this.state.animate && i === 0) ? "caruselCard newCard2" : "caruselCard"}>
-                                        <img className="caruselImage" src={this.state.preLoadImgDisplay[i]} alt='poster'></img>
+                                <div key={i}>
+                                    <div className={(this.state.animate && i === 0) ? "caruselCard cardMoveLeft" : "caruselCard"}>
+                                        <img className="caruselImage" src={this.state.preLoadImgDisplay[i] ? this.state.preLoadImgDisplay[i]: 'https://i.gifer.com/XVo6.gif'} alt='poster'></img>
                                         <div className="caruselTitle">{this.state.titles[i]}</div>
                                     </div></div>)
                         }
@@ -116,7 +122,7 @@ class CaruselComponent extends React.Component {
                 <button
                     type="button" className="caruselButtonNext"
                     onClick={() => {
-                        this.scrollContainerBy((this.container.scrollWidth / 20))
+
                     }}
                 >
                     Next
